5e5748725c3f9fd9af928d03c39f6b529e5238fd
carlaastrain <carlaastrain@gmail.com>
2023-04-12 15:39:35 -0700

    questions_section rewroted code

--- lib/widgets/questions_section.dart
+++ lib/widgets/questions_section.dart
@@ -5,60 +5,61 @@ import '../services/license_service.dart';
 import '../utils/app_styles.dart';
 
 class QuestionsSection extends StatefulWidget {
-  const QuestionsSection({super.key});
+  const QuestionsSection({Key? key}) : super(key: key);
 
   @override
   State<QuestionsSection> createState() => _QuestionsSectionState();
 }
 
 class _QuestionsSectionState extends State<QuestionsSection> {
-  final licenseService = GetIt.I<LicenseService>();
+  final LicenseService licenseService = GetIt.instance<LicenseService>();
+  int _selectedValue = 0;
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: licenseService.getLicenseTest1(),
-      builder: (context, snapshot) => !snapshot.hasData
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : Container(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 10),
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      snapshot.data![0].question,
-                      style: Styles.headlineStyle3
-                          .copyWith(color: Colors.grey.shade700),
-                    ),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final question = snapshot.data?[0].question ?? '';
+        final options = snapshot.data?[0].options ?? [];
+        return Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 10),
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  question,
+                  style: Styles.headlineStyle3.copyWith(
+                    color: Colors.grey.shade700,
                   ),
-                  RadioListTile(
-                    value: 1,
-                    groupValue: 1,
-                    onChanged: (value) {},
-                    title: Text(snapshot.data![0].options[0]["answer"],
-                        style: Styles.headlineStyle4),
-                  ),
-                  RadioListTile(
-                    value: 2,
-                    groupValue: 1,
-                    onChanged: (value) {},
-                    title: Text(snapshot.data![0].options[1]["answer"],
-                        style: Styles.headlineStyle4),
-                  ),
-                  RadioListTile(
-                    value: 3,
-                    groupValue: 1,
-                    onChanged: (value) {},
-                    title: Text(snapshot.data![0].options[2]["answer"],
-                        style: Styles.headlineStyle4),
-                  ),
-                ],
+                ),
               ),
-            ),
+              for (var i = 0; i < options.length; i++)
+                RadioListTile<int>(
+                  value: i + 1,
+                  groupValue: _selectedValue,
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedValue = value ?? 0;
+                    });
+                  },
+                  title: Text(
+                    options[i]['answer'] ?? '',
+                    style: Styles.headlineStyle4,
+                  ),
+                ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
