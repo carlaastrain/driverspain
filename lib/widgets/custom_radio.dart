2d56f2223c0820fd1aa8f39c83b5062c90796fd5
carlaastrain <carlaastrain@gmail.com>
2023-04-12 16:29:58 -0700

    adding custom radio buttions

---
+++ lib/widgets/custom_radio.dart
@@ -0,0 +1,60 @@
+import 'package:driver_spain_app/utils/app_layout.dart';
+import 'package:flutter/material.dart';
+
+class CustomRadio extends StatelessWidget {
+  final bool isSelected;
+  final String label;
+  final VoidCallback? onTap;
+  const CustomRadio(
+      {super.key, required this.isSelected, this.onTap, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: isSelected
+          ? Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  width: 22.0,
+                  height: 22.0,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.green,
+                  ),
+                  child: const Icon(
+                    Icons.check,
+                    size: 16.0,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  width: AppLayout.getScreenWidth() * 0.8,
+                  child: Text(label),
+                )
+              ],
+            )
+          : Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  width: 22.0,
+                  height: 22.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      width: 1.5,
+                      color: Colors.grey.shade700,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: AppLayout.getScreenWidth() * 0.8,
+                  child: Text(label),
+                )
+              ],
+            ),
+    );
+  }
+}
--- lib/widgets/questions_section.dart
+++ lib/widgets/questions_section.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:get_it/get_it.dart';
 
 import '../services/license_service.dart';
 import '../utils/app_styles.dart';
+import 'custom_radio.dart';
 
 class QuestionsSection extends StatefulWidget {
   const QuestionsSection({Key? key}) : super(key: key);
@@ -12,53 +14,71 @@ class QuestionsSection extends StatefulWidget {
 }
 
 class _QuestionsSectionState extends State<QuestionsSection> {
-  final LicenseService licenseService = GetIt.instance<LicenseService>();
-  int _selectedValue = 0;
+  final licenseService = GetIt.I<LicenseService>();
+  int _selectedValue = -1;
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: licenseService.getLicenseTest1(),
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Error: ${snapshot.error}'),
+          );
+        } else if (!snapshot.hasData) {
           return const Center(
             child: CircularProgressIndicator(),
           );
-        }
-        final question = snapshot.data?[0].question ?? '';
-        final options = snapshot.data?[0].options ?? [];
-        return Container(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(bottom: 10),
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  question,
-                  style: Styles.headlineStyle3.copyWith(
-                    color: Colors.grey.shade700,
+        } else {
+          return Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(bottom: 10),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    snapshot.data![0].question,
+                    style: Styles.headlineStyle3
+                        .copyWith(color: Colors.grey.shade700),
                   ),
                 ),
-              ),
-              for (var i = 0; i < options.length; i++)
-                RadioListTile<int>(
-                  value: i + 1,
-                  groupValue: _selectedValue,
-                  onChanged: (value) {
+                const Gap(15),
+                CustomRadio(
+                  isSelected: _selectedValue == 1,
+                  onTap: () {
                     setState(() {
-                      _selectedValue = value ?? 0;
+                      _selectedValue = 1;
                     });
                   },
-                  title: Text(
-                    options[i]['answer'] ?? '',
-                    style: Styles.headlineStyle4,
-                  ),
+                  label: snapshot.data![0].options[0]['answer'],
                 ),
-            ],
-          ),
-        );
+                const Gap(15),
+                CustomRadio(
+                  isSelected: _selectedValue == 2,
+                  onTap: () {
+                    setState(() {
+                      _selectedValue = 2;
+                    });
+                  },
+                  label: snapshot.data![0].options[1]['answer'],
+                ),
+                const Gap(15),
+                CustomRadio(
+                  isSelected: _selectedValue == 3,
+                  onTap: () {
+                    setState(() {
+                      _selectedValue = 3;
+                    });
+                  },
+                  label: snapshot.data![0].options[2]['answer'],
+                ),
+              ],
+            ),
+          );
+        }
       },
     );
   }
